810e565e9458a1a9967e1d35e594160c249740bb
Hasan Karahan <hasan.karahan81@gmail.com>
2013-05-09 23:35:12 +0300

    Decoupled from ZMQ and enhanced CLI argument handling

--- ticker-sim.py
+++ ticker-sim.py
@@ -11,7 +11,7 @@ __author__ = 'hsk81'
 import json as JSON
 import argparse
 import time
-import zmq
+import sys
 
 from dateutil import parser
 from datetime import datetime
@@ -19,56 +19,60 @@ from datetime import datetime
 ###############################################################################
 ###############################################################################
 
-def get_arguments ():
+def get_arguments () -> argparse.Namespace:
 
     parser = argparse.ArgumentParser (description=
-        "Publishes recorded ticks for simulation and training purposes with "
+        "Loops over recorded ticks for simulation and training purposes with "
         "the possibility to adjust the tick speed as desired.")
 
-    parser.add_argument ("-s", "--silent",
+    parser.add_argument ("-v", "--verbose",
         default=False, action="store_true",
-        help="skip CLI logging (default: %(default)s)")
-    parser.add_argument ("-f", "--filename",
-        default='log/ticks.log',
-        help="file to read records from (default: %(default)s)")
-    parser.add_argument ("-b", "--from-date",
+        help="verbose logging (default: %(default)s)")
+    parser.add_argument ('-o', '--out-keys',
+        default=['timestamp', 'bid', 'ask', 'price', 'high', 'low', 'volume'],
+        nargs='+', help='output keys (default: %(default)s)')
+    parser.add_argument ('-m', '--map-to', action='append',
+        default=[('last', 'price')],
+        nargs='+', help='output maps (default: %(default)s)')
+    parser.add_argument ("-f", "--from-date",
         default=None,
         help="date to begin from (default: %(default)s)")
-    parser.add_argument ("-e", "--to-date",
+    parser.add_argument ("-t", "--to-date",
         default=None,
         help="date to stop at (default: %(default)s)")
     parser.add_argument ("-a", "--acceleration",
         default=1.000, type=float,
-        help="time acceleration factor (default: %(default)s)")
-    parser.add_argument ("-pub", "--pub-address",
-        default='tcp://*:7000',
-        help="ticker publication address (default: %(default)s)")
+        help="time acceleration (default: %(default)s)")
 
     return parser.parse_args ()
 
-def loop (socket, filename, from_date, to_date, acceleration, silent=True):
+def loop (out_keys: list, out_maps: dict,
+          from_date: datetime, to_date: datetime,
+          acceleration: float, verbose: bool=False) -> None:
 
-    from_date = parser.parse (from_date) if from_date else None
-    to_date = parser.parse (to_date) if to_date else None
+    def mapped (key: str) -> str:
+        return out_maps[key] if key in out_maps else key
 
-    with open (filename, 'r') as file:
+    def select (tick: dict) -> dict: return {
+        mapped (k): v for k, v in tick.items () if mapped (k) in out_keys
+    }
 
-        curr_time = None
-        for line in file:
+    curr_tick = None
+    for line in sys.stdin:
 
-            last_time = curr_time
-            curr_tick = JSON.loads (line)
-            curr_time = datetime.fromtimestamp (curr_tick['timestamp'])
+        last_tick, curr_tick = curr_tick, JSON.loads (line.replace ("'", '"'))
+        now = datetime.fromtimestamp (curr_tick['timestamp'])
+        if from_date and from_date > now: continue
+        if to_date and to_date <= now: break
 
-            if from_date and from_date > curr_time: continue
-            if to_date and to_date <= curr_time: break
+        if last_tick is not None:
+            curr_time = float (curr_tick['timestamp'])
+            last_time = float (last_tick['timestamp'])
+            time.sleep ((curr_time - last_time) * acceleration)
 
-            if last_time is not None:
-                diff_secs = curr_time.timestamp () - last_time.timestamp ()
-                time.sleep (diff_secs * acceleration)
-
-            socket.send_string (line)
-            if not silent: print ('[%s] %s' % (curr_time, curr_tick))
+        tick = select (curr_tick)
+        if verbose: print ('[%s] %s' % (now, tick), file=sys.stderr)
+        print (tick, file=sys.stdout)
 
 ###############################################################################
 ###############################################################################
@@ -76,16 +80,15 @@ def loop (socket, filename, from_date, to_date, acceleration, silent=True):
 if __name__ == "__main__":
 
     args = get_arguments ()
-    context = zmq.Context (1)
-    socket = context.socket (zmq.PUB)
-    socket.bind (args.pub_address)
+    args.map_to = dict (args.map_to)
+    args.from_date = parser.parse (args.from_date) if args.from_date else None
+    args.to_date = parser.parse (args.to_date) if args.to_date else None
 
-    try:
-        loop (socket, args.filename, args.from_date, args.to_date,
-              args.acceleration, silent=args.silent)
+    try: loop (args.out_keys, args.map_to, args.from_date, args.to_date,
+        args.acceleration, verbose=args.verbose)
 
-    except KeyboardInterrupt: pass
-    finally: socket.close ()
+    except KeyboardInterrupt:
+        pass
 
 ###############################################################################
 ###############################################################################
