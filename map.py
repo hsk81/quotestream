a51066951e132760b1ba3b23b2e27f242dfc0415
Hasan Karahan <hasan.karahan81@gmail.com>
2013-05-13 00:33:32 +0300

    Interpret `op` as map

--- map.py
+++ map.py
@@ -12,8 +12,8 @@ import ujson as JSON
 import argparse
 import sys
 
-from functools import reduce
 from datetime import datetime
+from functools import reduce
 from numpy import *
 
 ###############################################################################
@@ -22,23 +22,23 @@ from numpy import *
 def get_arguments () -> argparse.Namespace:
 
     parser = argparse.ArgumentParser (description=
-        "Evaluates provided functions which can have multiple parameters and "
-        "stores the results in the corresponding keys. If there are less "
-        "functions than result keys than the last one will be used for the "
-        "remaining results. The parameters for each function need to be "
-        "provided as a group. Empty groups can either be left out or they can "
-        "be indicated by an empty string (used with functions without "
-        "parameters). String interpolation is based on Python's `format`.")
+        "Maps functions to a set of parameter values and stores the results "
+        "in the corresponding keys. If there are less functions than result "
+        "keys than the last one will be repeated for the remaining results. "
+        "The parameters for each function need to be provided as a group. "
+        "Empty groups can either be left out or they can be indicated by an "
+        "empty string (used for functions without parameters). Interpolation "
+        "of parameters is based on the string's `format` method.")
 
     parser.add_argument ('-v', '--verbose',
         default=False, action='store_true',
         help='verbose logging (default: %(default)s)')
+    parser.add_argument ('-f', '--function', action='append',
+        default=[], nargs='+',
+        help='map function(s) (default: %(default)s)')
     parser.add_argument ('-p', '--parameter-group', action='append',
         default=[], nargs='+',
         help='parameter group *per* result key (default: %(default)s)')
-    parser.add_argument ('-f', '--function', action='append',
-        default=[], nargs='+',
-        help='function(s) to evaluate (default: %(default)s)')
     parser.add_argument ('-r', '--result', action='append',
         default=[], nargs='+',
         help='result keys (default: %(default)s)')
@@ -51,8 +51,10 @@ def loop (functions: list, parameter_groups: list, results: list,
     for line in sys.stdin:
         tick = JSON.decode (line.replace ("'", '"'))
 
-        for pg, res, fn in zip (parameter_groups, results, functions):
-            tick[res] = eval (fn.format (*map (lambda p: tick[p], pg)))
+        for item in zip (functions, parameter_groups, results):
+            function, parameter_group, result = item
+            tick[result] = eval (function.format (*map (
+                lambda parameter: tick[parameter], parameter_group)))
 
         if verbose:
             now = datetime.fromtimestamp (tick['timestamp'])
