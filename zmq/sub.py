371f18f657056486b0d5098b6d1ec470c19e8a39
Hasan Karahan <hasan.karahan81@gmail.com>
2013-05-31 15:15:32 +0300

    Ensured multiple subscriptions

--- zmq/sub.py
+++ zmq/sub.py
@@ -14,19 +14,28 @@ import zmq
 import sys
 
 from datetime import datetime
+from functools import reduce
 
 ###############################################################################
 ###############################################################################
 
 def get_arguments () -> argparse.Namespace:
-
     parser = argparse.ArgumentParser ()
+
+    class attach (argparse.Action):
+        """Appends values by *overwriting* initial defaults (if any)"""
+        def __call__(self, parser, namespace, values, option_string=None):
+            items = getattr (namespace, self.dest, [])
+            if items == self.default:
+                setattr (namespace, self.dest, [values])
+            else:
+                setattr (namespace, self.dest, list (items) + [values])
 
     parser.add_argument ("-v", "--verbose",
         default=False, action="store_true",
         help="verbose logging (default: %(default)s)")
     parser.add_argument ('-sub', '--sub-address',
-        default='tcp://127.0.0.1:8888',
+        default=[['tcp://127.0.0.1:8888']], action=attach, nargs='+',
         help='subscription address (default: %(default)s)')
 
     return parser.parse_args ()
@@ -34,10 +43,10 @@ def get_arguments () -> argparse.Namespace:
 ###############################################################################
 ###############################################################################
 
-def loop (context: zmq.Context, address: str, verbose: bool=False) -> None:
+def loop (context: zmq.Context, addresses: list, verbose: bool=False) -> None:
 
     socket = context.socket (zmq.SUB)
-    socket.connect (address)
+    for address in addresses: socket.connect (address)
     socket.LINGER = 0
 
     socket.setsockopt_string (zmq.SUBSCRIBE, '')
@@ -56,13 +65,23 @@ def loop (context: zmq.Context, address: str, verbose: bool=False) -> None:
 ###############################################################################
 ###############################################################################
 
+def normalize (args: argparse.Namespace) -> argparse.Namespace:
+
+    args.sub_address = list (reduce (lambda a, b: a + b, args.sub_address, []))
+    return args
+
+###############################################################################
+###############################################################################
+
 if __name__ == "__main__":
 
-    args = get_arguments ()
+    args = normalize (get_arguments ())
     context = zmq.Context (1)
 
-    try: loop (context, args.sub_address, verbose=args.verbose)
-    except KeyboardInterrupt: pass
+    try:
+        loop (context, args.sub_address, verbose=args.verbose)
+    except KeyboardInterrupt:
+        pass
 
 ###############################################################################
 ###############################################################################
