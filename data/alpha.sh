6e56fed107eb60ade96752bb6cd21f21f4dd4739
Hasan Karahan <hasan.karahan81@gmail.com>
2013-09-03 17:53:44 +0500

    Updated simulation results w.r.t. `alpha` strategy

--- data/alpha.sh
+++ data/alpha.sh
@@ -1,13 +1,45 @@
-cat log/ticks.log | ./filter.py -e high low -e bid ask -e volume | ./map/float.py -p last -r last | ./map/log.py -p last -r last | ./simulate.py -a 0.001 | ./zmq/pub.py -v > /dev/null
+#!/usr/bin/env sh
 
-./zmq/sub.py | ./interpolate.py -i 1.200 | ./reduce/return.py -p last -r return -n 500 | ./reduce/volatility.py -p return -r volatility -n 500 | ./alias.py -m volatility lhs-volatility | ./zmq/pub.py -pub 'tcp://*:7777' -v > /dev/null
+##
+## => {'timestamp': .., 'last': [117.77], 'log': [4.76]}
+##
 
-./zmq/sub.py | ./interpolate.py -i 1.000 | ./reduce/return.py -p last -r return -n 600 | ./reduce/volatility.py -p return -r volatility -n 600 | ./alias.py -m volatility rhs-volatility | ./zmq/pub.py -pub 'tcp://*:9999' -v > /dev/null
+cat log/ticks-32k.log | ./py filter -e high low -e bid ask -e volume | ./py map.float -p last | ./py map.log -p last | ./py sim -a 0.001 | ./py zmq.push -a 'ipc:///tmp/0' -v > /dev/null
 
-./zmq/sub.py -sub 'tcp://127.0.0.1:7777' -sub 'tcp://127.0.0.1:9999' | ./reduce/ratio.py -n lhs-volatility -d rhs-volatility -r ratio | ./zmq/pub.py -pub 'tcp://*:7799' -v > /dev/null
+##
+## => {'timestamp': .., 'last': [117.77], 'log': [4.76], 'diff': [0.0033], 'lhs-vola': [0.68]}
+##
 
-./zmq/sub.py -sub 'tcp://127.0.0.1:7799' | grep "rhs-volatility" | ./alias.py -m rhs-volatility volatility -v > data/lrv-ratio.log
+./py zmq.sub -a 'ipc:///tmp/0' | ./py interpolate -i 2.5 -v | ./py reduce.diff -p log -n 240 | ./py reduce.vola -p diff -r lhs-vola -n 240 > /tmp/ticks.lhs
 
-cat data/lrv-ratio.log | ./map/exp.py -p last -r price | ./trade/alpha-sim.py -v > data/alpha-sim.log
+##
+## => {'timestamp': .., 'last': [117.77], 'log': [4.76], 'diff': [0.0033], 'rhs-vola': [0.76]}
+##
 
-tail -n 1024 log/ticks.log | ./filter.py -i timestamp -i last | ./map/float.py -p last -r last | ./map/log.py -p last -r last | ./reduce/ema.py -p last -r ema -v | ./plot.py -p timestamp last -p last -p timestamp ema -p ema -n 2 > /dev/null
+./py zmq.sub -a 'ipc:///tmp/0' | ./py interpolate -i 3.0 -v | ./py reduce.dilog -n 200 | ./py reduce.vola -p diff -r rhs-vola -n 200 > /tmp/ticks.rhs
+
+
+##
+## => {'timestamp': .., 'last': [117.74], 'log': [4.76], 'diff': [0.0012], 'lhs-vola': [0.59], 'div': [0.54]}
+##
+
+cat /tmp/ticks.lhs | ./py zmq.push -a 'ipc:///tmp/0.lhs'
+cat /tmp/ticks.rhs | ./py zmq.push -a 'ipc:///tmp/0.rhs'
+
+./py zmq.pull -a 'ipc:///tmp/0.lhs' -a 'ipc:///tmp/0.rhs' | ./py interleave.divp lhs-vola rhs-vola -v > /tmp/ticks.div
+
+##
+## => {'timestamp': .., 'price': [117.8], 'log': [4.76], 'return': [0.0018], 'rhs-vola': [1.07], 'ratio': [1.15],
+##     'btc': [0.09], 'tot': [100.76], 'usd': [89.43]}
+##
+
+cat /tmp/ticks.div | grep "rhs-vola" | ./py alias -m last price | ./py alias -m diff return | ./py alias -m div ratio | ./py strategy.alpha --fee=0.000 -v > /tmp/ticks.000
+
+##
+## => PNG: {(timestamp,log)   ; (timestamp,usd),
+##          (timestamp,tot)   ; (timestamp,btc),
+##          (timestamp,ratio) ; (timestamp,return)}
+##
+
+cat /tmp/ticks.000 | ./graph/plot.py -p timestamp log -p timestamp usd -p timestamp tot -p timestamp btc -p timestamp ratio -p timestamp return -n 2 > /dev/null
+
