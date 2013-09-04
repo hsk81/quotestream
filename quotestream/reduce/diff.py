4aeec500484b034139070c4ab95a4b6eab079f63
Hasan Karahan <hasan.karahan81@gmail.com>
2013-09-04 12:02:27 +0500

    Generalized `reduce.diff` to multiple parameters

--- quotestream/reduce/diff.py
+++ quotestream/reduce/diff.py
@@ -19,8 +19,8 @@ class DiffCallable (object):
     def __init__ (self, n: int) -> None:
         self.n = n
 
-    def __call__ (self, values: numpy.array, last=None) -> numpy.array:
-        return values[0] - values[self.n - 1]
+    def __call__ (self, *arrays: [numpy.array], last=None) -> numpy.array:
+        return numpy.array ([arr[0] - arr[self.n - 1] for arr in arrays])
 
     def __repr__ (self):
         return '@{0} - @{n-1}'
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     diff = DiffCallable (n=2)
 
     parser = do.get_args_parser ({
-        'stack-size': diff.n, 'function': diff, 'result': 'diff'
+        'stack-size': diff.n, 'function': diff, 'default': [], 'result': 'diff'
     })
 
     parser.description = \
@@ -64,6 +64,10 @@ if __name__ == "__main__":
 
     args = do.get_args (parser=parser)
     diff.n = args.stack_size
+
+    if isinstance (args.default, list):
+        dsz = len (args.parameters) - len (args.default)
+        for _ in range (dsz): args.default.append (0.0)
 
     try: do.loop (args.function, args.parameters, args.stack_size,
         args.default, args.result, verbose=args.verbose)
