hsk81/fx-bitstamp

Introduced `interval-scaled` option (which can be overridden by the `scale` option) to have a simpler way to change the scaling factor; plus documented briefly.

reduce/volatility.py
@@ -33,7 +33,7 @@ class VolatilityCallable (object):
 
         return power * self.scale
 
-    def __repr__ (self): return '{0} - {n-1}'
+    def __repr__ (self): return 'volatility for dt=1s, n=600 and p=2.0'
 
 ###############################################################################
 ###############################################################################
@@ -47,17 +47,52 @@ if __name__ == "__main__":
         'stack-size': [[600]], ## == 10 min., for 1 sec. interpolation
     })
 
-    parser.add_argument ("-e", "--exponent", default=2.0, type=float,
+    ##
+    ## `volatility.exponent`
+    ## ---------------------
+    ## A large exponent `p` gives more weight to the tails of the distribution.
+    ## If `p` is too large, the realized volatility may have an asymptotically
+    ## infinite expectation (if returns have a heavy-tailed density function.
+    ##
+
+    parser.add_argument ("-e", "--exponent",
+        default=2.0, type=float,
         help="p-exponent (default: %(default)s)")
-    parser.add_argument ("-s", "--scale", default=None, type=float,
-        help="scaling factor (default: %(default)s)")
+
+    ##
+    ## `volatility.scale`
+    ## ------------------
+    ## It is possible to get the volatility in *scaled* form: Although the
+    ## volatility may be computed from e.g. 10-min returns, the expected vola-
+    ## tility over another time interval (e.g. 1 hour or year) may also be
+    ## calculated. Through a Gaussian scaling law, `v^2 <- dt`, the following
+    ## definition of scaled volatility is obtained:
+    ##
+    ##     v_scaled = square-root {dt_scaled / dt} * v.
+    ##
+
+    parser.add_argument ("-s", "--scale",
+        default=None, type=float,
+        help="scaling factor; overrides -i (default: %(default)s)")
+
+    ##
+    ## `volatility.interval-scaled`
+    ## ----------------------------
+    ## The most popular choice of the scaling reference interval `dt_scale` is
+    ## 1 year. If this is chosen, then `v_scaled` is called an *annualized*
+    ## volatility.
+    ##
+
+    parser.add_argument ("-i", "--interval-scaled",
+        default=3600.0 * 24.0 * 365.0, type=float,
+        help="scaled interval (default: %(default)s [s])")
 
     args = do.get_args (parser=parser)
     args = do.normalize (args)
 
     volatility.exponent, volatility.scale = args.exponent, \
-        numpy.sqrt (numpy.array (3600.0 * 24.0 * 365.0) / args.stack_size) \
-            if args.scale is None else args.scale
+        numpy.sqrt (numpy.array (args.interval_year) / args.stack_size) \
+            if args.scale is None else args.scale ## override `interval-scaled`
 
     try: do.loop (args.function, args.parameter, args.stack_size, args.default,
         args.result, verbose=args.verbose)

hsk81/quotestream

reduce/volatility.py
@@ -33,7 +33,7 @@ class VolatilityCallable (object):
 
         return power * self.scale
 
-    def __repr__ (self): return '{0} - {n-1}'
+    def __repr__ (self): return 'volatility for dt=1s, n=600 and p=2.0'
 
 ###############################################################################
 ###############################################################################
@@ -47,17 +47,52 @@ if __name__ == "__main__":
         'stack-size': [[600]], ## == 10 min., for 1 sec. interpolation
     })
 
-    parser.add_argument ("-e", "--exponent", default=2.0, type=float,
+    ##
+    ## `volatility.exponent`
+    ## ---------------------
+    ## A large exponent `p` gives more weight to the tails of the distribution.
+    ## If `p` is too large, the realized volatility may have an asymptotically
+    ## infinite expectation (if returns have a heavy-tailed density function.
+    ##
+
+    parser.add_argument ("-e", "--exponent",
+        default=2.0, type=float,
         help="p-exponent (default: %(default)s)")
-    parser.add_argument ("-s", "--scale", default=None, type=float,
-        help="scaling factor (default: %(default)s)")
+
+    ##
+    ## `volatility.scale`
+    ## ------------------
+    ## It is possible to get the volatility in *scaled* form: Although the
+    ## volatility may be computed from e.g. 10-min returns, the expected vola-
+    ## tility over another time interval (e.g. 1 hour or year) may also be
+    ## calculated. Through a Gaussian scaling law, `v^2 <- dt`, the following
+    ## definition of scaled volatility is obtained:
+    ##
+    ##     v_scaled = square-root {dt_scaled / dt} * v.
+    ##
+
+    parser.add_argument ("-s", "--scale",
+        default=None, type=float,
+        help="scaling factor; overrides -i (default: %(default)s)")
+
+    ##
+    ## `volatility.interval-scaled`
+    ## ----------------------------
+    ## The most popular choice of the scaling reference interval `dt_scale` is
+    ## 1 year. If this is chosen, then `v_scaled` is called an *annualized*
+    ## volatility.
+    ##
+
+    parser.add_argument ("-i", "--interval-scaled",
+        default=3600.0 * 24.0 * 365.0, type=float,
+        help="scaled interval (default: %(default)s [s])")
 
     args = do.get_args (parser=parser)
     args = do.normalize (args)
 
     volatility.exponent, volatility.scale = args.exponent, \
-        numpy.sqrt (numpy.array (3600.0 * 24.0 * 365.0) / args.stack_size) \
-            if args.scale is None else args.scale
+        numpy.sqrt (numpy.array (args.interval_scaled) / args.stack_size) \
+            if args.scale is None else args.scale ## override `interval-scaled`
 
     try: do.loop (args.function, args.parameter, args.stack_size, args.default,
         args.result, verbose=args.verbose)