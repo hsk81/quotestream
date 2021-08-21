hsk81/fx-bitstamp

Removed ZMQ, using plain `stdout` and more systematic argument processing

ticker.py
@@ -11,62 +11,74 @@ __author__ = 'hsk81'
 import requests as req
 import argparse
 import time
-import zmq
+import sys
 
 from datetime import datetime
 
 ###############################################################################
 ###############################################################################
 
-def get_arguments ():
+def get_arguments () -> argparse.Namespace:
 
     parser = argparse.ArgumentParser (description=
-        "Polls exchange for new ticks: The poll interval limits the maximum "
-        "possible tick resolution, so keeping it as low as possible is "
-        "desired. But since the exchange does impose a request limit per time "
-        "unit it's not possible to poll beyond that cap (without getting "
-        "banned). The ticks are published for further processing.")
+        'Polls exchange for new ticks: The poll interval limits the maximum '
+        'possible tick resolution, so keeping it as low as possible is '
+        'desired. But since the exchange does impose a request limit per time '
+        'unit it\'s not possible to poll beyond that cap (without getting '
+        'banned).')
 
-    parser.add_argument ("-s", "--silent",
-        default=False, action="store_true",
-        help="skip CLI logging (default: %(default)s)")
-    parser.add_argument ("-i", "--poll-interval",
+    parser.add_argument ('-v', '--verbose',
+        default=False, action='store_true',
+        help='verbose logging (default: %(default)s)')
+    parser.add_argument ('-o', '--out-keys',
+        default=['bid', 'ask', 'price', 'high', 'low', 'volume'],
+        nargs='+', help='output keys (default: %(default)s)')
+    parser.add_argument ('-a', '--alias-map', action='append',
+        default=[('last', 'price')  ],
+        nargs='+', help='alias map (default: %(default)s)')
+    parser.add_argument ('-i', '--interval',
         default=1.250, type=float,
-        help="seconds between ticker polls (default: %(default)s [s])")
-    parser.add_argument ("-pub", "--pub-address",
-        default='tcp://*:7000',
-        help="ticker publication address (default: %(default)s)")
-    parser.add_argument ("-u", "--ticker-url",
+        help='seconds between polls (default: %(default)s [s])')
+    parser.add_argument ('-u', '--url',
         default='https://www.bitstamp.net/api/ticker/',
-        help="API (default: %(default)s)")
+        help='API (default: %(default)s)')
 
     return parser.parse_args ()
 
-def get_tick (url):
+def next_response (url: str) -> req.Response:
 
     res = req.get (url); return res if res.status_code == 200 else None
 
-def loop (socket, poll_interval, ticker_url, silent=True):
+def loop (out_keys: list, alias_map: dict, interval: float, url: str,
+          verbose: bool=False) -> None:
 
-    last_tick = None
+    def alias (key: str) -> str:
+        return alias_map[key] if key in alias_map else key
+
+    def select (tick: dict) -> dict: return {
+        alias (k): v for k, v in tick.items () if alias (k) in out_keys
+    }
+
     t0 = time.time ()
+    last_response = None
 
     while True:
-        this_tick = get_tick (ticker_url)
-        if this_tick:
+        curr_response = next_response (url)
+        if curr_response:
+            if not last_response or last_response.text != curr_response.text:
 
-            timestamp = time.time ()
-            now = datetime.fromtimestamp (timestamp)
-            if not last_tick or last_tick.text != this_tick.text:
+                inp_tick = curr_response.json ()
+                inp_tick['timestamp'] = time.time ()
+                out_tick = select (inp_tick)
 
-                tick = this_tick.json ()
-                if not silent: print ('[%s] %s' % (now, tick))
-                tick['timestamp'] = timestamp
-                socket.send_json (tick)
+                if verbose:
+                    now = datetime.fromtimestamp (inp_tick['timestamp'])
+                    print ('[%s] %s' % (now, out_tick), file=sys.stderr)
 
-            last_tick = this_tick
+                print (out_tick, file=sys.stdout)
+            last_response = curr_response
 
-        dt = poll_interval - (time.time () - t0)
+        dt = interval - (time.time () - t0)
         if dt > 0.000: time.sleep (dt)
         t0 = time.time ()
 
@@ -76,15 +88,12 @@ def loop (socket, poll_interval, ticker_url, silent=True):
 if __name__ == "__main__":
 
     args = get_arguments ()
-    context = zmq.Context (1)
-    socket = context.socket (zmq.PUB)
-    socket.bind (args.pub_address)
 
-    try:
-        loop (socket, args.poll_interval, args.ticker_url, silent=args.silent)
+    try: loop (args.out_keys, dict (args.alias_map), args.interval, args.url,
+        verbose=args.verbose)
 
-    except KeyboardInterrupt: pass
-    finally: socket.close ()
+    except KeyboardInterrupt:
+        pass
 
 ###############################################################################
 ###############################################################################

hsk81/quotestream

ticker.py
@@ -11,62 +11,74 @@ __author__ = 'hsk81'
 import requests as req
 import argparse
 import time
-import zmq
+import sys
 
 from datetime import datetime
 
 ###############################################################################
 ###############################################################################
 
-def get_arguments ():
+def get_arguments () -> argparse.Namespace:
 
     parser = argparse.ArgumentParser (description=
-        "Polls exchange for new ticks: The poll interval limits the maximum "
-        "possible tick resolution, so keeping it as low as possible is "
-        "desired. But since the exchange does impose a request limit per time "
-        "unit it's not possible to poll beyond that cap (without getting "
-        "banned). The ticks are published for further processing.")
+        'Polls exchange for new ticks: The poll interval limits the maximum '
+        'possible tick resolution, so keeping it as low as possible is '
+        'desired. But since the exchange does impose a request limit per time '
+        'unit it\'s not possible to poll beyond that cap (without getting '
+        'banned).')
 
-    parser.add_argument ("-s", "--silent",
-        default=False, action="store_true",
-        help="skip CLI logging (default: %(default)s)")
-    parser.add_argument ("-i", "--poll-interval",
+    parser.add_argument ('-v', '--verbose',
+        default=False, action='store_true',
+        help='verbose logging (default: %(default)s)')
+    parser.add_argument ('-o', '--out-keys',
+        default=['timestamp', 'bid', 'ask', 'price', 'high', 'low', 'volume'],
+        nargs='+', help='output keys (default: %(default)s)')
+    parser.add_argument ('-m', '--map-to', action='append',
+        default=[('last', 'price')],
+        nargs='+', help='output maps (default: %(default)s)')
+    parser.add_argument ('-i', '--interval',
         default=1.250, type=float,
-        help="seconds between ticker polls (default: %(default)s [s])")
-    parser.add_argument ("-pub", "--pub-address",
-        default='tcp://*:7000',
-        help="ticker publication address (default: %(default)s)")
-    parser.add_argument ("-u", "--ticker-url",
+        help='seconds between polls (default: %(default)s [s])')
+    parser.add_argument ('-u', '--url',
         default='https://www.bitstamp.net/api/ticker/',
-        help="API (default: %(default)s)")
+        help='API (default: %(default)s)')
 
     return parser.parse_args ()
 
-def get_tick (url):
+def next_response (url: str) -> req.Response:
 
     res = req.get (url); return res if res.status_code == 200 else None
 
-def loop (socket, poll_interval, ticker_url, silent=True):
+def loop (out_keys: list, map_to: dict, interval: float, url: str,
+          verbose: bool=False) -> None:
 
-    last_tick = None
+    def mapped (key: str) -> str:
+        return map_to[key] if key in map_to else key
+
+    def select (tick: dict) -> dict: return {
+        mapped (k): v for k, v in tick.items () if mapped (k) in out_keys
+    }
+
     t0 = time.time ()
+    last_response = None
 
     while True:
-        this_tick = get_tick (ticker_url)
-        if this_tick:
+        curr_response = next_response (url)
+        if curr_response:
+            if not last_response or last_response.text != curr_response.text:
 
-            timestamp = time.time ()
-            now = datetime.fromtimestamp (timestamp)
-            if not last_tick or last_tick.text != this_tick.text:
+                inp_tick = curr_response.json ()
+                inp_tick['timestamp'] = time.time ()
+                out_tick = select (inp_tick)
 
-                tick = this_tick.json ()
-                if not silent: print ('[%s] %s' % (now, tick))
-                tick['timestamp'] = timestamp
-                socket.send_json (tick)
+                if verbose:
+                    now = datetime.fromtimestamp (inp_tick['timestamp'])
+                    print ('[%s] %s' % (now, out_tick), file=sys.stderr)
 
-            last_tick = this_tick
+                print (out_tick, file=sys.stdout)
+            last_response = curr_response
 
-        dt = poll_interval - (time.time () - t0)
+        dt = interval - (time.time () - t0)
         if dt > 0.000: time.sleep (dt)
         t0 = time.time ()
 
@@ -76,15 +88,13 @@ def loop (socket, poll_interval, ticker_url, silent=True):
 if __name__ == "__main__":
 
     args = get_arguments ()
-    context = zmq.Context (1)
-    socket = context.socket (zmq.PUB)
-    socket.bind (args.pub_address)
+    args.map_to = dict (args.map_to)
 
-    try:
-        loop (socket, args.poll_interval, args.ticker_url, silent=args.silent)
+    try: loop (args.out_keys, args.map_to, args.interval, args.url,
+        verbose=args.verbose)
 
-    except KeyboardInterrupt: pass
-    finally: socket.close ()
+    except KeyboardInterrupt:
+        pass
 
 ###############################################################################
 ###############################################################################